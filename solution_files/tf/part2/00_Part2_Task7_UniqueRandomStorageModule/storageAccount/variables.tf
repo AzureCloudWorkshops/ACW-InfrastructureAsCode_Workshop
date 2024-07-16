0bc6000c40d72bd1b763d4a99d18afd6acc5c950
Brian L. Gorman <blgorman@gmail.com>
2024-07-16 09:01:02 -0500

    part 1 step 7 complete with a couple typo fixes

---
+++ solution_files/tf/part2/00_Part2_Task7_UniqueRandomStorageModule/storageAccount/variables.tf
@@ -0,0 +1,20 @@
+variable "storageAccountNameFull" {
+    type = string
+    nullable = false
+}
+
+variable "resourceGroupName" {
+    type = string
+    nullable = false
+}
+
+variable "location" {
+    type = string
+    nullable = false
+    default = "East US"
+
+    validation {
+      condition = contains(["East US"], var.location)
+      error_message = "Resources can only be deployed to East US location."
+    }
+}
---
+++ solution_files/tf/part2/00_Part2_Task7_UniqueRandomStorageModule/variables.tf
@@ -0,0 +1,49 @@
+variable "resourceGroupName" {
+    type = string
+    nullable = false
+}
+
+variable "location" {
+    type = string
+    nullable = false
+    default = "East US"
+
+    validation {
+      condition = contains(["East US"], var.location)
+      error_message = "Resources can only be deployed to East US location."
+    }
+}
+
+
+variable "storageAccountNameBase" {
+    type = string
+    nullable = false
+    default = "iacstg"
+
+    validation {
+      condition = length(var.storageAccountNameBase) > 3
+      error_message = "The storage account name should be at least 3 characters"
+    }
+}
+
+variable "uniqueIdentifier" {
+    type = string
+    nullable = false
+    default = "20291231acw"
+
+    validation {
+      condition = length(var.uniqueIdentifier) == 11
+      error_message = "The unique identifier should be 11 characters"
+    }
+}
+
+variable "environment" {
+    type = string
+    nullable = false
+    default = "dev"
+
+    validation {
+      condition = contains(["dev", "prod"], var.environment)
+      error_message = "The environment should be dev or prod"
+    }
+}
